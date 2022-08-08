c95db5cdc87b412b18153195f6d7be064f29faa2
PaxDaciae <80334035+PaxDaciae@users.noreply.github.com>
2022-08-08 17:06:56 +0300

    React

---
+++ cardgen/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <p> no meanie </p>
+  <div className = "app-container">
+    <div className = "card-output">
+      <div className = "card-container">
+
+        
+      </div>
+    
+    </div>
+    {/* <hr class = "card-divider"></hr> */}
+    <br />
+    <button>aalal</button>
+  </div>
+  
+  </>
+  );
+}
+
+export default App;
